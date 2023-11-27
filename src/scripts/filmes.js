cc183b42a653c0afef7c4ca41224fce97531cb4c
A97Campos <andrewleandro97@gmail.com>
2023-11-27 10:09:03 -0300

    final do projeto

--- src/scripts/filmes.js
+++ src/scripts/filmes.js
@@ -1,21 +1,17 @@
 const minions = document.getElementById('minions');
 const meuMalvadoFavorito = document.getElementById('meuMalvadoFavorito');
+const filmesMinions = document.getElementById('filmesMinions');
+const filmesMeuMalvadoFavorito = document.getElementById('filmesMeuMalvadoFavorito');
 
 minions.addEventListener("click", function(){
-    let filmesMinions = document.getElementById('filmesMinions');
-    let filmesMeuMalvadoFavorito = document.getElementById('filmesMeuMalvadoFavorito');
-
-    filmesMinions.style.display = 'grid';
+    filmesMinions.style.display = 'flex';
     filmesMeuMalvadoFavorito.style.display = 'none';
     meuMalvadoFavorito.classList.remove('is-active');
     minions.classList.add('is-active');
 });
 
 meuMalvadoFavorito.addEventListener("click", function(){
-    let filmesMinions = document.getElementById('filmesMinions');
-    let filmesMeuMalvadoFavorito = document.getElementById('filmesMeuMalvadoFavorito');
-
-    filmesMeuMalvadoFavorito.style.display = 'grid';
+    filmesMeuMalvadoFavorito.style.display = 'flex';
     filmesMinions.style.display = 'none';
     meuMalvadoFavorito.classList.add('is-active');
     minions.classList.remove('is-active');
